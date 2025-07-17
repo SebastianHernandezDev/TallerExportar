5c118b5a099d794b0c828a8dda6d38e787a01ce4
sebastian david hernandez <96744129+SebastianHernandezDev@users.noreply.github.com>
2025-07-16 23:25:12 -0500

    Delete Modulos directory

--- Modulos/app.js
+++
@@ -1,8 +0,0 @@
-import {guardarUsuario,traemeTodos} from "./db.js"
-
-
-guardarUsuario("andres");
-guardarUsuario("Marta");
-guardarUsuario("Santi");
-guardarUsuario("andrea");
-console.log(traemeTodos);
