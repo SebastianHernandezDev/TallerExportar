38e18844f40fa2d9b9a0c25015ec4d2af9990e51
sebastian david hernandez <96744129+SebastianHernandezDev@users.noreply.github.com>
2025-07-16 23:24:52 -0500

    Delete EStructurasDeDatos directory

--- EStructurasDeDatos/scope.js
+++
@@ -1,9 +0,0 @@
-//Variable global
-const Variableglobal = "me conocen en todo lado"
-
-function scope(){
-    // variable loca
-    const variablelocal = "solo me conocen aqui"
-}
-
-console.log( Variableglobal);
